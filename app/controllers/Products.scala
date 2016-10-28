616307597db0382adeddb7dd61a7dcf2e3a4a9de
Soyoun Kim <makeajourney@gmail.com>
2016-10-28 17:29:49 +0900

    2.3.2 form objects

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -2,7 +2,9 @@ package controllers
 
 import play.api.mvc._
 import javax.inject.Inject
-import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.i18n.{Messages, MessagesApi, I18nSupport}
+import play.api.data.Form
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 import models.Product
 
 class Products @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
@@ -17,4 +19,12 @@ class Products @Inject() (val messagesApi: MessagesApi) extends Controller with 
       product => Ok(views.html.products.details(product))
     }.getOrElse(NotFound)  // return page 404
   }
+
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),  // form의 field와 제약사항
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply) // form과 model 사이 mapping
+  )
 }
