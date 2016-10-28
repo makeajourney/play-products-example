06d088a3a440dc40b0e5395f7a20148ed412f011
Soyoun Kim <makeajourney@gmail.com>
2016-10-28 18:03:56 +0900

    2.3.5 사용자 입력에 대한 유효성 검증

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -27,4 +27,29 @@ class Products @Inject() (val messagesApi: MessagesApi) extends Controller with 
       "description" -> nonEmptyText
     )(Product.apply)(Product.unapply) // form과 model 사이 mapping
   )
+
+  def save = Action {
+    implicit request => val newProductForm = productForm.bindFromRequest()
+
+    newProductForm.fold(
+      hasErrors = {
+        form => Redirect(routes.Products.newProduct()).
+          flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+      },
+      success = {
+        newProduct => Product.add(newProduct)
+        val message = Messages("products.new.success", newProduct.name)
+        Redirect(routes.Products.show(newProduct.ean)).flashing("success" -> message)
+      }
+    )
+  }
+
+  def newProduct = Action {
+    implicit request => val form = if (request2flash.get("error").isDefined)
+      productForm.bind(request2flash.data)
+    else
+      productForm
+
+    Ok(views.html.products.editProduct(form))
+  }
 }
